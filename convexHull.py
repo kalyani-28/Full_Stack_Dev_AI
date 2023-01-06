8872697fbd0c7fdd8af48bfee5f578e48ca99589
BenSuth <sutherland681@gmail.com>
2023-01-05 19:23:57 -0500

    feat: isolated words in an image
    before we can process handwriting using an OCR, we need to isolated and find the position of each word in the image. This feat adds the ability to isolate each word (and its position) using the findTextLabels function in convexHull.py

--- convexHull.py
+++ convexHull.py
@@ -3,7 +3,10 @@ from typing import List
 import cv2
 from cv2 import LINE_AA
 import numpy as np
+import json
 
+path = 'results/ROI{0}.png'
+filename = 'ROI.json'
 
 def segmentImage(img):
     """Binarize image"""
@@ -65,8 +68,116 @@ def boundingBox(contours: List):
     """Get bounding box for each contour"""
     return [cv2.boundingRect(c) for c in contours]
 
+# given an image and a list of rectangles
+# crop the image using each rectangle
+# returns a json object with the rectangles and path to each cropped image
+def getROI(img, rectangles: List):
+    obj = {}
+    image = img.copy()
 
-if __name__ == "__main__":
+    # iterate through contours
+    for i, c in enumerate(rectangles):
+        x = c[0]
+        y = c[1]
+        w = c[2] 
+        h = c[3]
+
+        # for each contour found, crop the image and save a copy based on bounding box
+        ROI = image[y:y+h, x:x+w]
+        cv2.imwrite(path.format(i), ROI)
+
+        # each contour will be added to a dictionary
+        tmp = {
+            "xmin": x,
+            "ymin": y,
+            "xmax": w,
+            "ymax": h,
+            "path": path.format(i)
+        }
+        obj[i] = tmp
+
+    return obj
+
+# given an image and a list of rectangles
+# merge rectangles that are close (both horizontally and vertically)
+def mergeRects(img, rects):
+    rectsUsed = []
+    acceptedRects = []
+
+    # merge threshold
+    xThr = 30
+    yThr = 20
+
+
+    # add padding to final bounding boxes in case edge letters are cut
+    xpadding = 10
+    ypadding = 10
+    
+    for _ in rects:
+        rectsUsed.append(False)
+
+    # debug
+    # textLabelsImg = img.copy()
+    # for rect in rects:
+    #     cv2.rectangle(textLabelsImg, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (121, 11, 189), 2)
+    # cv2.imwrite("results/convexHull_textLabelBoxes.png", textLabelsImg)
+
+    def getXFromRect(item):
+        return item[0]
+
+    # sort by mininum x of each rectangle
+    rects.sort(key = getXFromRect)
+
+    # reference: https://stackoverflow.com/questions/55376338/how-to-join-nearby-bounding-boxes-in-opencv-python
+    # Iterate all initial bounding rects
+    for supIdx, supVal in enumerate(rects):
+        if (rectsUsed[supIdx] == False):
+            # Initialize current rect
+            currxMin = supVal[0]
+            currxMax = supVal[0] + supVal[2]
+            curryMin = supVal[1]
+            curryMax = supVal[1] + supVal[3]
+
+            # This bounding rect is used
+            rectsUsed[supIdx] = True
+
+            # Iterate all initial bounding rects
+            # starting from the next
+            for subIdx, subVal in enumerate(rects[(supIdx+1):], start = (supIdx+1)):
+                # Initialize merge candidate
+                candxMin = subVal[0]
+                candxMax = subVal[0] + subVal[2]
+                candyMin = subVal[1]
+                candyMax = subVal[1] + subVal[3]
+
+                if (candyMax > curryMax + yThr):
+                    continue
+
+                if (candyMin < curryMin - yThr):
+                    continue
+
+                # Check if x distance between current rect
+                # and merge candidate is small enough
+                if (candxMin <= currxMax + xThr):
+
+                    # Reset coordinates of current rect
+                    currxMax = candxMax
+                    curryMin = min(curryMin, candyMin)
+                    curryMax = max(curryMax, candyMax)
+
+                    # Merge candidate (bounding rect) is used
+                    rectsUsed[subIdx] = True
+                else:
+                    break
+
+            # No more merge candidates possible, accept current rect
+            # Use the x threshold to filter out small area rectangles
+            # if (currxMin + xThr < currxMax):
+            acceptedRects.append([currxMin - xpadding, curryMin - ypadding, currxMax - currxMin + xpadding, curryMax - curryMin + ypadding])
+    
+    return acceptedRects
+
+def findAllLabels():
     # provide image file as cli argument
     imgFile = sys.argv[1]
     # read image
@@ -135,3 +246,54 @@ if __name__ == "__main__":
 
     print(textBoxBounds)
    #  cv2.imwrite("results/convexHull_boundingTextBoxes", textBoxBoundingImg)
+
+# returns json object containing each minimum bounding box of a word in the image
+def findTextLabels(imgFile):
+    # read image
+    img = cv2.imread(imgFile)
+
+    # segment image
+    segmentedImg = segmentImage(img)
+
+    # find contours in image
+    contours = getContours(segmentedImg)
+    # approximage the contours to smoothen lines
+    approxContours = approximateContours(contours)
+
+    # find convex hull of approximated contours
+    hulls = getHulls(approxContours)
+
+    # find hulls containing text boxes and text labels
+    _, textLabels = filterTextBoxes(hulls)
+
+    # write image containing text labels
+    # textLabelsImg = img.copy()
+    # cv2.drawContours(textLabelsImg, textLabels, -1,
+    #                  (255, 0, 0), thickness=1, lineType=cv2.LINE_AA)
+    # cv2.imwrite("results/convexHull_textLabels.png", textLabelsImg)
+
+    # find the minimum bounding box for each rectangle
+    rects = []
+    for contour in textLabels:
+        rect = cv2.boundingRect(contour)
+        rects.append(rect)
+
+    # firstRound = mergeRects(img, rects)
+    acceptedRects = mergeRects(img, rects)
+    
+    # debug
+    textLabelsImg = img.copy()
+    for rect in acceptedRects:
+        result = cv2.rectangle(textLabelsImg, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (121, 11, 189), 2)
+    cv2.imwrite("results/convexHull_results.png", result)
+
+    # crop the image for each final rectangle
+    obj = getROI(img, acceptedRects)
+    print(json.dumps(obj, indent=4))
+
+    return obj
+
+if __name__ == "__main__":
+    # provide image file as cli argument
+    imgFile = sys.argv[1]
+    findTextLabels(imgFile)

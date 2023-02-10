3aab7538344ff18d7eaaf9d035b44141b40283ab
BenSuth <sutherland681@gmail.com>
2023-02-10 18:17:08 -0500

    add calculation for midpoint of bounding boxes

--- src/imageProcessEngine/Image.py
+++ src/imageProcessEngine/Image.py
@@ -1,10 +1,24 @@
 import json
+from typing import Dict, Tuple
+
+def getMidPoint(x1, x2, y1, y2) -> Tuple[int, int]:
+    midx = round((x1 + x2)/2)
+    midy = round((y1 + y2)/2)
+    return (midx, midy)
+
+def processCoordinates(coordinates) -> Dict:
+    for i in coordinates:
+        coordinates[i]["midpoint"] = getMidPoint(coordinates[i]["xmin"],
+                                                 coordinates[i]["xmax"], 
+                                                 coordinates[i]["ymin"], 
+                                                 coordinates[i]["ymax"])
+    return coordinates
 
 class Image:
     def __init__(self, imgPath, elements, labels) -> None:
         self.imgPath = imgPath
-        self.labels = labels
-        self.elements = elements
+        self.labels = processCoordinates(labels)
+        self.elements = processCoordinates(elements)
         self.mapping = {}
 
     def prettyPrint(self) -> None:
